72538882e8eece31ef5068e00d4ec4f1a1c3f909
Sonia Q <sonyaq@gmail.com>
2018-05-22 18:35:07 -0500

    comments

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
+
+function getQuote() {
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayQuote(data.message);
+    })
+    .catch(function () {
+      console.log("An error occurred");
+    });
+}
+
+function displayQuote(quote) {
+  var quoteText = document.querySelector('.quote-text');
+  quoteText.textContent = quote;
+
+var tweetButton = document.querySelector('.tweet');
+   tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
+  
+}
+
+var newQuoteButton = document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+getQuote();
